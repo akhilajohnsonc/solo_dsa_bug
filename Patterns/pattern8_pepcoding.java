83a688e94b2c177ad7f4007a6a3d8711bf30b26d
akhilajohnsonc <59962882+akhilajohnsonc@users.noreply.github.com>
2023-08-20 23:59:47 +0530

    Update pattern8_pepcoding.java

--- Patterns/pattern8_pepcoding.java
+++ Patterns/pattern8_pepcoding.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 /*1. You are given a number n.
 2. You've to create a pattern of * and separated by tab as shown in output format.
 5
-        *
-      *
-    *
-  *
-*
+        * 
+      * 
+    * 
+  * 
+* 
 */
 
 public class pattern8_pepcoding {
@@ -16,17 +16,17 @@ public class pattern8_pepcoding {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
-		int star = n;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
-				if (j == star) {
-					System.out.print("*");
+				if (j + i == n + 1) {
+					System.out.print("* ");
 					break;
+				} else {
+					System.out.print("  ");
 				}
-				System.out.print("  ");
 			}
 			System.out.println();
-			star--;
+
 		}
 
 	}
